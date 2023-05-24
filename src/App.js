d3e7f17ba0cbc9d3541d5a13606c0b8ebf793553
Daniel Tapasco <daniel.tapasco@ucp.edu.co>
2023-05-23 20:26:45 -0500

    Prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to Prueba Desarrolloo.
+          Edit <code>src/App.js</code> and save to Prueba Desao.
         </p>
         <a
           className="App-link"
